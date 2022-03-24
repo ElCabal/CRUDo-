26f2aa22fe056e385667f49dcdaa71a2722f2555
ElCabal <cabalholguinlufe@gmail.com>
2022-03-24 18:16:01 -0500

    inicio de proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        baloo: ["Baloo Bhaijaan 2", "cursive"],
+        title: ["Montserrat", "sans-serif"],
+        subtitle: ["Work Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};

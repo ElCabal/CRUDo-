fc37c94a6ccd63e2f42e87387f91687c5bdfb280
ElCabal <cabalholguinlufe@gmail.com>
2022-03-25 17:42:41 -0500

    changes section navbar and page landing

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MainLayout from "../sections/MainLayout";
 
 const Landing = () => {
-    return (
-        <div className="flex flex-col justify-center items-center p-2 w-full gap-6">
-            <h1 className="font-primary font-black text-gray-900 text-5xl text-center">
-                Bienvenido a CRUDo
-            </h1>
-            <p className="text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-                reiciendis accusantium ipsam vitae fugit, itaque repellendus non
-                qui numquam repudiandae facere aliquam veniam nobis quibusdam
-                deserunt autem incidunt praesentium? Dignissimos.
-            </p>
-            <div className="flex gap-2 w-full">
-                <Link to="/newdate" className="btn btn-primary">CREAR CITA</Link>
-                <Link to="/datelist" className="btn btn-secondary">VER CITAS</Link>
-            </div>
+  return (
+    <MainLayout title="Bienvenido a CRUDo">
+      <div className="flex flex-col justify-center items-center p-2 w-full gap-6">
+        {/* <h1 className="font-primary font-black text-gray-900 text-5xl text-center">
+          Bienvenido a CRUDo
+        </h1> */}
+        <p className="text-center text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          reiciendis accusantium ipsam vitae fugit, itaque repellendus non qui
+          numquam repudiandae facere aliquam veniam nobis quibusdam deserunt
+          autem incidunt praesentium? Dignissimos.
+        </p>
+        <div className="flex gap-2 w-full">
+          <Link to="/newdate" className="btn btn-primary">
+            CREAR CITA
+          </Link>
+          <Link to="/datelist" className="btn btn-secondary">
+            VER CITAS
+          </Link>
         </div>
-    );
+      </div>
+    </MainLayout>
+  );
 };
 
 export default Landing;
--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -3,28 +3,66 @@ import { BiMenu } from "react-icons/bi";
 import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <nav className="p-4 flex justify-between items-center w-full relative">
-            <Link to="/" className="font-secondary font-black text-5xl text-sky-700 cursor-pointer">
-                CRUDo
-            </Link>
-            <BiMenu onClick={()=> setIsOpen(!isOpen)} className="text-gray-900 text-5xl cursor-pointer active:scale-95" />
-            { isOpen &&
-                <div className="absolute flex flex-col justify-center items-center bg-gray-900 text-sky-600 font-montserrat font-black w-full gap-4 py-4 top-full left-0">
-                    <NavLink onClick={()=> setIsOpen(false)} to="/" className={({ isActive }) => (isActive ? 'text-white' : 'inactive')}>
-                        INICIO
-                    </NavLink>
-                    <NavLink onClick={()=> setIsOpen(false)} to="/datelist" className={({ isActive }) => (isActive ? 'text-white' : 'inactive')}>
-                        CITAS
-                    </NavLink>
-                    <NavLink onClick={()=> setIsOpen(false)} to="/newdate" className={({ isActive }) => (isActive ? 'text-white' : 'inactive')}>
-                        NUEVA CITA
-                    </NavLink>
-                </div>
-            }
-        </nav>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="py-4 px-5 flex justify-between items-center w-full relative lg:px-20">
+      <Link
+        to="/"
+        className="font-secondary font-black text-5xl text-sky-700 cursor-pointer"
+      >
+        CRUDo
+      </Link>
+      <BiMenu
+        onClick={() => setIsOpen(!isOpen)}
+        className="text-gray-900 text-5xl cursor-pointer active:scale-95 lg:hidden"
+      />
+      {isOpen && (
+        <div className="absolute flex flex-col justify-center items-center bg-gray-900 text-sky-600 font-montserrat font-black w-full gap-4 py-4 top-full left-0">
+          <NavLink
+            onClick={() => setIsOpen(false)}
+            to="/"
+            className={({ isActive }) => (isActive ? "text-white" : "inactive")}
+          >
+            INICIO
+          </NavLink>
+          <NavLink
+            onClick={() => setIsOpen(false)}
+            to="/datelist"
+            className={({ isActive }) => (isActive ? "text-white" : "inactive")}
+          >
+            CITAS
+          </NavLink>
+          <NavLink
+            onClick={() => setIsOpen(false)}
+            to="/newdate"
+            className={({ isActive }) => (isActive ? "text-white" : "inactive")}
+          >
+            NUEVA CITA
+          </NavLink>
+        </div>
+      )}
+      <div className="hidden gap-20 font-black lg:flex ">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "text-sky-600" : "inactive")}
+        >
+          INICIO
+        </NavLink>
+        <NavLink
+          to="/datelist"
+          className={({ isActive }) => (isActive ? "text-sky-600" : "inactive")}
+        >
+          CITAS
+        </NavLink>
+        <NavLink
+          to="/newdate"
+          className={({ isActive }) => (isActive ? "text-sky-600" : "inactive")}
+        >
+          NUEVA CITA
+        </NavLink>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;

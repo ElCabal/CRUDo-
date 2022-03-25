8b827486214acbadf5f54f2b7653e47c097de1fb
iElectro <ejose.acc@gmail.com>
2022-03-24 22:36:13 -0500

    Buttons done

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Landing = () => {
-  return <div>Landing</div>;
+    return (
+        <div className="p-2">
+            <div className="flex gap-2">
+                <button className="btn btn-primary">CREAR CITA</button>
+                <button className="btn btn-secondary">VER CITAS</button>
+            </div>
+        </div>
+    );
 };
 
 export default Landing;

7b2fcd327f9abbaf1973cc857b837689585203a4
ElCabal <cabalholguinlufe@gmail.com>
2022-03-24 23:48:36 -0500

    page create date

--- src/components/pages/NewDate.jsx
+++ src/components/pages/NewDate.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import MainLayout from "../sections/MainLayout";
 
 const NewDate = () => {
-  return <div>NewDate</div>;
+  return (
+    <MainLayout title="Crear Cita">
+      <div className="bg-white w-full p-8 rounded shadow-xl">
+        <form
+          action=""
+          className="flex flex-col gap-8 justify-center items-center"
+        >
+          <div className="flex flex-col w-full gap-2">
+            <label
+              htmlFor="name"
+              className=" font-semibold font-primary text-2xl"
+            >
+              Nombre:
+            </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Ingresar nombre"
+              className="bg-gray-100 p-2 rounded placeholder-gray-400 text-gray-700"
+              autoComplete="off"
+            />
+          </div>
+          <div className="flex flex-col w-full gap-2">
+            <label
+              htmlFor="topic"
+              className=" font-semibold font-primary text-2xl "
+            >
+              Asunto:
+            </label>
+            <input
+              type="text"
+              id="topic"
+              placeholder="Ingresar asunto"
+              className="bg-gray-100 p-2 rounded placeholder-gray-400 text-gray-700"
+              autoComplete="off"
+            />
+          </div>
+          <div className="flex flex-col w-full gap-2">
+            <label
+              htmlFor="date"
+              className=" font-semibold font-primary text-2xl "
+            >
+              Fecha:
+            </label>
+            <input
+              type="date"
+              id="date"
+              className="bg-gray-100 p-2 rounded placeholder-gray-400"
+            />
+          </div>
+          <div className="flex gap-2 w-full">
+            <button className="btn btn-primary">GUARDAR</button>
+            <button className="btn btn-danger">CANCELAR</button>
+          </div>
+        </form>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default NewDate;
--- src/components/sections/MainLayout.jsx
+++ src/components/sections/MainLayout.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const MainLayout = ({children, title}) => {
+const MainLayout = ({ children, title }) => {
   return (
-    <div className="flex flex-col justify-center items-center gap-4">
-        <h2 className="font-primary font-black text-4xl">{title}</h2>
-        {children}
+    <div className="flex flex-col justify-center items-center gap-4 md:gap-8 px-2 w-full md:max-w-2xl lg:max-w-4xl">
+      <h2 className="font-primary font-black text-4xl md:text-5xl lg:text-6xl">
+        {title}
+      </h2>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;

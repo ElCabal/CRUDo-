58fb2a1eb2096ef2714fba49c055d84b96bd0322
ElCabal <cabalholguinlufe@gmail.com>
2022-03-28 11:01:29 -0500

    BASE URL modified to deploy

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://127.0.0.1:8000/apirest-crudo/api/date.php"
-export const ID = "?id="
+export const BASE_URL = "https://api-crudo.herokuapp.com/api/date.php";
+export const ID = "?id=";

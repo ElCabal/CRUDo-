dc04e1a6481a76bf36b4bd5d2630facb768e8f11
ElCabal <cabalholguinlufe@gmail.com>
2022-03-28 16:26:56 -0500

    resolve route e404

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import Navbar from "./components/sections/Navbar";
 import Footer from "./components/sections/Footer";
-import {Route, Routes} from "react-router-dom";
-import Landing from "./components/pages/Landing";
-import DateList from "./components/pages/DateList";
-import EditDate from "./components/pages/EditDate";
-import NewDate from "./components/pages/NewDate";
-import Error404 from "./components/pages/Error404";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  
   return (
     <div className="bg-gray-100 min-h-screen gap-5 flex flex-col justify-between items-center font-secondary text-gray-700">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-          <Route path="datelist" element={<DateList />} />
-          <Route path="editdate/:id_param" element={<EditDate />} />
-          <Route path="newdate" element={<NewDate />} />
-          <Route path="*" element={<Error404 />} />
-
-      </Routes>
-      <Footer />  
+      <Outlet />
+      <Footer />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,24 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Landing from "./components/pages/Landing";
+import EditDate from "./components/pages/EditDate";
+import DateList from "./components/pages/DateList";
+import NewDate from "./components/pages/NewDate";
+import Error404 from "./components/pages/Error404";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index="home" element={<Landing />} />
+          <Route path="datelist" element={<DateList />} />
+          <Route path="editdate/:id_param" element={<EditDate />} />
+          <Route path="newdate" element={<NewDate />} />
+        </Route>
+        <Route path="*" element={<Error404 />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
